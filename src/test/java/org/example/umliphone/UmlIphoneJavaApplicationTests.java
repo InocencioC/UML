3e6e0c3b15a578625aefa96380823afd01df609c
Inocencio Cordeiro <cordeiroturorias@gmail.com>
2024-08-04 22:32:38 +0100

    first commit

---
+++ src/test/java/org/example/umliphone/UmlIphoneJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.umliphone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmlIphoneJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
